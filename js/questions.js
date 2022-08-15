ba5efa4c7e59f34d7ecd01a2bdc52d391a36e418
Darpan Kumar Mittal <dmittal_be20@thapar.edu>
2022-08-15 22:35:17 +0530

    myquizapp

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does the MP stand for in MP3?",
+    answer: "Moving Picture",
+    options: [
+      "Music Player",
+      "Multi Pass",
+      "Micro Point",
+      "Moving Picture"
+    ]
+  },
+  {
+    numb: 2,
+    question: "When Gmail first launched, how much storage did it provide for your email?",
+    answer: "1GB",
+    options: [
+      "512MB",
+      "1GB",
+      "5GB",
+      "Unlimited"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following function of String object creates an HTML anchor that is used as a hypertext target?",
+    answer: "anchor()",
+    options: [
+      "anchor()",
+      "link()",
+      "blink()",
+      "big()"
+    ]
+  },
+  {
+    numb: 4,
+    question: "How long is an IPv6 address?",
+    answer: "128 bits",
+    options: [
+      "128 bits",
+      "32 bits",
+      "64 bits",
+      "128 bytes"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which built-in method returns the calling string value converted to lower case?",
+    answer: "toLowerCase()",
+    options: [
+      "toLowerCase()",
+      "toLower()",
+      "changeCase(case)",
+      "None of the above."
+    ]
+  },
+
+];
